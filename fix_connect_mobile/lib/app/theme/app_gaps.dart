812dbea5226e1b188963f0e5ffcb13a256affc2c
Daniel Okoro <danielochinasa@gmail.com>
2026-01-23 08:39:12 +0100

    feat: Refactor app theming with updated color constants, spacing, and text styles

---
+++ fix_connect_mobile/lib/app/theme/app_gaps.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/widgets.dart';
+import 'app_spacing.dart';
+
+class AppGaps {
+  static const hXs = SizedBox(height: AppSpacing.xs);
+  static const hSm = SizedBox(height: AppSpacing.sm);
+  static const hMd = SizedBox(height: AppSpacing.md);
+  static const hLg = SizedBox(height: AppSpacing.lg);
+  static const hXl = SizedBox(height: AppSpacing.xl);
+
+  static const wXs = SizedBox(width: AppSpacing.xs);
+  static const wSm = SizedBox(width: AppSpacing.sm);
+  static const wMd = SizedBox(width: AppSpacing.md);
+}
--- fix_connect_mobile/lib/features/auth/presentation/pages/login_page.dart
+++ fix_connect_mobile/lib/features/auth/presentation/pages/login_page.dart
@@ -1,3 +1,6 @@
+import 'package:fix_connect_mobile/app/theme/app_gaps.dart';
+import 'package:fix_connect_mobile/app/theme/app_text_styles.dart';
+import 'package:fix_connect_mobile/app/theme/app_theme.dart';
 import 'package:fix_connect_mobile/app/theme/theme_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -16,8 +19,11 @@ class LoginPage extends StatelessWidget {
             children: [
               Column(
                 children: [
-                  Text('Fix Connect', style: TextStyle(fontSize: 24)),
-                  SizedBox(height: 8),
+                  Text(
+                    'Fix Connect',
+                    style: Theme.of(context).textTheme.titleLarge,
+                  ),
+                  AppGaps.hXs,
                   Text(
                     'Welcome back to your creative',
                     style: TextStyle(fontSize: 16),
