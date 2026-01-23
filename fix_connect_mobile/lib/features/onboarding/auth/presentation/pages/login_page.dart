29b3256836132836b2ca8bd8678d41542b7525a4
Daniel Okoro <danielochinasa@gmail.com>
2026-01-23 14:32:47 +0100

    feat: Add app navigation system with route management and navigator key

--- fix_connect_mobile/lib/features/onboarding/auth/presentation/pages/login_page.dart
+++ fix_connect_mobile/lib/features/onboarding/auth/presentation/pages/login_page.dart
@@ -1,6 +1,4 @@
 import 'package:fix_connect_mobile/app/theme/app_gaps.dart';
-import 'package:fix_connect_mobile/app/theme/app_text_styles.dart';
-import 'package:fix_connect_mobile/app/theme/app_theme.dart';
 import 'package:fix_connect_mobile/app/theme/theme_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
