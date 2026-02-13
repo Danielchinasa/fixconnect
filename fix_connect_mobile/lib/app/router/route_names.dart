89ed98c0781e6e5c66dc6092f6fc15479fb4cc9b
Daniel Okoro <danielochinasa@gmail.com>
2026-02-13 14:46:08 +0100

    feat: Implement signup page and navigation from login page

--- fix_connect_mobile/lib/app/router/route_names.dart
+++ fix_connect_mobile/lib/app/router/route_names.dart
@@ -1,7 +1,9 @@
 class AppRoutes {
   static const onboarding = '/';
   static const login = '/login';
+  static const signUp = '/signup';
 
   static String loginPage() => login;
   static String onboardingScreen() => onboarding;
+  static String signUpPage() => signUp;
 }
